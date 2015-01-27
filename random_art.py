090cb883d26df9c23eafa89bcb6f1b698583a89d
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-27 09:03:30 -0500

    Made things artsy.

--- random_art.py
+++ random_art.py
@@ -18,6 +18,9 @@ def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
     function_list = []
+    function_list.append(lambda x, y: sqrt(abs(y) + 1))
+    function_list.append(lambda x, y: avg(sin(x) * cos(pi *y)))
+    function_list.append(lambda x, y: avg(sin(x), cos(y)))
     function_list.append(lambda x, y: sin(pi * (sin(y /2) * 2) - 1 + cos(x) * geometric_mean(x, y)))
     function_list.append(lambda x, y: sin(x * sin(y)) + avg(0.5, x))
     expr = random.choice(function_list)
