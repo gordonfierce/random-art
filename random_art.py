2390ec29be677834b3e5ef2cbb1d0639b74ee5cf
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-26 23:13:21 -0500

    Introduce randomness to picture.

--- random_art.py
+++ random_art.py
@@ -1,14 +1,26 @@
 import random
+from math import sin, cos, pi, sqrt
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
 # Your expression should have a __str__() function defined for it.
 
+def geometric_mean(*args):
+    multiplicative_sum = 1
+    for arg in args:
+        multiplicative_sum *= (abs(arg) +1)
+    return pow(multiplicative_sum, -len(args))
+
+def avg(*args):
+    return sum(args) / len(args)
 
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr = lambda x, y: (random.random() * 2) - 1
+    function_list = []
+    function_list.append(lambda x, y: sin(pi * (sin(y /2) * 2) - 1 + cos(x) * geometric_mean(x, y)))
+    function_list.append(lambda x, y: sin(x * sin(y)) + avg(0.5, x))
+    expr = random.choice(function_list)
     return expr
 
 
@@ -16,4 +28,5 @@ def run_expression(expr, x, y):
     """This function takes an expression created by create_expression and
     an x and y value. It runs the expression, passing the x and y values
     to it and returns a value between -1.0 and 1.0."""
+
     return expr(x, y)
